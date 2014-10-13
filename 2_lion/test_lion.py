02d50d4805876b0ce8c77c8ff360da7b135bb45a
beta-tank <beta-tank@outlook.com>
2014-10-13 19:00:22 +0700

    Edit tests

--- 2_lion/test_lion.py
+++ 2_lion/test_lion.py
@@ -18,16 +18,21 @@ states = [s.hungry, s.full]
 
 
 class LionTest(unittest.TestCase):
-    # Создание льва
-    def test_lion_creation(self):
+    # Создание голодного льва
+    def test_lion_hungry_creation(self):
         try:
             lion = l.Lion(s.hungry)
             self.assertEqual(s.hungry, lion.state, 'Wrong state of created lion')
+            self.assertEqual(None, lion.action, 'Wrong action of created lion')
         except ValueError:
             self.fail("Exception during lion creation")
+
+    # Создание сытого льва
+    def test_lion_full_creation(self):
         try:
             lion = l.Lion(s.full)
             self.assertEqual(s.full, lion.state, 'Wrong state of created lion')
+            self.assertEqual(None, lion.action, 'Wrong action of created lion')
         except ValueError:
             self.fail("Exception during lion creation")
 
@@ -36,14 +41,23 @@ class LionTest(unittest.TestCase):
         self.assertRaises(ValueError, l.Lion, None)
         self.assertRaises(ValueError, l.Lion, object)
 
-    # Подача корректных объектов
-    def test_input_objects(self):
-
+    # Подача корректных объектов голодному льву
+    def test_input_objects_on_hungry_lion(self):
         lion = l.Lion(s.hungry)
+        objects = [o.hunter, o.hunter, o.tree, o.tree, o.hunter, o.antelope, o.antelope]
         try:
-            for i in range(0, 50):
-                j = r.randint(0, len(inputs) - 1)
-                lion.meet(inputs[j])
+            for ob in objects:
+                lion.meet(ob)
+        except:
+            self.fail('Exception on correct input')
+
+    # Подача корректных объектов сытому льву
+    def test_input_objects_on_full_lion(self):
+        lion = l.Lion(s.full)
+        objects = [o.antelope, o.hunter, o.hunter, o.antelope, o.tree, o.antelope, o.antelope]
+        try:
+            for ob in objects:
+                lion.meet(ob)
         except:
             self.fail('Exception on correct input')
 
@@ -56,21 +70,19 @@ class LionTest(unittest.TestCase):
 
     # Проверка правильности перехода по состояниям
     def test_states(self):
-        for state in states:
-            for input_object in inputs:
-                lion = l.Lion(state)
-                lion.meet(input_object)
-                true_state, none = automate[(state, input_object)]
-                self.assertEqual(true_state, lion.state, 'Wrong lion state')
+        for state, input_object in automate:
+            lion = l.Lion(state)
+            lion.meet(input_object)
+            true_state, none = automate[(state, input_object)]
+            self.assertEqual(true_state, lion.state, 'Wrong lion state')
 
     # Проверка правильности реакции на объекты
     def test_actions(self):
-        for state in states:
-            for input_object in inputs:
-                lion = l.Lion(state)
-                lion.meet(input_object)
-                none, true_action = automate[(state, input_object)]
-                self.assertEqual(true_action, lion.action, 'Wrong lion state')
+        for state, input_object in automate:
+            lion = l.Lion(state)
+            lion.meet(input_object)
+            none, true_action = automate[(state, input_object)]
+            self.assertEqual(true_action, lion.action, 'Wrong lion state')
 
 
 if __name__ == '__main__':
